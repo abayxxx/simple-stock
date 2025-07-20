8a7037fc0bf61747e459582f1be938f90bb59cd3
abayxxx <taufik9018@gmail.com>
2025-07-20 16:47:46 +0700

    initial commit

--- resources/views/stocks/in/create.blade.php
+++ resources/views/stocks/in/create.blade.php
@@ -8,9 +8,8 @@
 @section('content')
 <form action="{{ route('stock.store', ['type' => $type]) }}" method="POST">
     @csrf
-    @include('stocks.partials.form', ['products' => $products, 'stock' => $stock ?? null, 'type' => $type])
+    @include('stocks.partials.form', ['products' => $products, 'stock' => $stock ?? null])
     <button type="submit" class="btn btn-primary">Simpan</button>
     <a href="{{ route('stock.' . $type) }}" class="btn btn-secondary">Kembali</a>
 </form>
-
 @stop
