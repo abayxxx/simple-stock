eba5ff5c81921d9577eaa7f7a060e545df0a7ded
abayxxx <taufik9018@gmail.com>
2025-08-16 17:03:38 +0700

    hotfix:(fix) dockerfile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,22 +15,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $admin = User::factory()->create([
+        $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
             'role' => 'admin', // Set role for the admin user
             'password' => bcrypt('password'), // Set a default password
         ]);
 
-        $superAdmin = User::factory()->create([
+        $superAdmin = User::create([
             'name' => 'Super Admin User',
             'email' => 'superadmin@example.com',
             'role' => 'superadmin', // Set role for the super admin user
             'password' => bcrypt('superadmin'), // Set a default password
         ]);
 
-        //Seeder products 5000 data
-       Product::factory()->count(5000)->create();
 
     }
 }
